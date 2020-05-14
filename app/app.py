7b035d05ea75cf09a5ca4b53e54a832ad7b4de54
José Luís <joseluis@jlmachine.localdomain>
2020-05-14 02:54:00 +0100

    added flash messages

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, flash
+
 from app.extensions import database
 from app.extensions.database import db
 from app.extensions import appearance
 from app.models.tables import Todo
-
 
 app = Flask(__name__)
 app.config.from_object('settings')
@@ -17,19 +17,18 @@ def index():
     if request.method == 'POST':
         task_content = request.form['content']
         new_task = Todo(content=task_content)
-        try:
+        if new_task:
             db.session.add(new_task)
             db.session.commit()
+            flash('Added task named as {}.'.format(new_task.content))
             return redirect('/')
-        except:
-            return "There was an issue adding a task"
-    else:
-        tasks = Todo.query.order_by(Todo.data_created).all()
-        return render_template('index.html', tasks=tasks)
+        return "There was an issue adding a task"
+    tasks = Todo.query.order_by(Todo.data_created).all()
+    return render_template('index.html', tasks=tasks)
+
 
 @app.route('/delete/<int:id>')
 def delete(id):
-
     task = Todo.query.get_or_404(id)
     try:
         db.session.delete(task)
@@ -38,9 +37,9 @@ def delete(id):
     except:
         return "Something went wrong while deleting"
 
+
 @app.route('/update/<int:id>', methods=['POST','GET'])
 def update(id):
-
     task = Todo.query.get_or_404(id)
     if request.method == 'POST':
         task.content = request.form['content']
@@ -51,6 +50,7 @@ def update(id):
             return "There was an issue during the update"
     else:
         return render_template('update.html', task=task)
+
 
 def create_app():
     return app
--- settings.py
+++ settings.py
@@ -1,2 +1,3 @@
 SQLALCHEMY_DATABASE_URI = 'sqlite:///todo.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+SECRET_KEY = '\xbf\x95j\x11\xecv\xf5\xb8Jc\xef\xa0\xd1P\xccq\xfa\xf6\xb4\xd2)\x12\x1b\xe0'
