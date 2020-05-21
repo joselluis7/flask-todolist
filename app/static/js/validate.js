a035bb65443de455c3dfc0d202e90e3d8d5aac1a
José Luís <zeluis0712@gmail.com>
2020-05-21 23:23:19 +0100

    Update validate.js

--- app/static/js/validate.js
+++ app/static/js/validate.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     $('#submitButton').click(function(){
         if ($('#contentInput').val() == ""){
-            console.log($('#contentInput').val())
             $('#validateModal').modal('toggle')
             return false
         }else{
